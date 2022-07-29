ffa8081a060a63d1fc35992c3b48c7f33b87dbf3
Pedro Victor <pedro20002victor@gmail.com>
2022-07-28 21:52:05 -0300

    Promise all

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-let searchCep = fetch('https://viacep.com.br/ws/01001000/json/')
-   // Toda resposta que chega, e do tipo response
-   
-   .then(
+async function searchCepFN(cepData) {
+   try {
+      let searchCep = await fetch(`https://viacep.com.br/ws/${cepData}/json/`)
+      // Toda resposta que chega, e do tipo response
+
       // Converte os dados que sao retornados em bytes
-      res => res.json()
-   )
-   .then(
-      // E referente ao dado devolvido na promise anterior
-      data => console.log(data)
-   )
-   .catch(
+      let conversionJson = await searchCep.json()
 
-   )
+      if (conversionJson.erro) {
+         throw Error('Adress does not exist');
+      }
 
-console.log(searchCep);
+      console.log(conversionJson);
+      return conversionJson;
+
+   } catch (erro) {
+      console.log(erro);
+   } 
+}
+
+const ceps = ['35700284', '35700263', '35700212' ]
+const conjunto = ceps.map( values => searchCepFN(values))
+Promise.all(conjunto).then(res => console.log(res))
