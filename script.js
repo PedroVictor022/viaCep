fc078ef86d8eb3701c66811320b43120b2ca5159
Pedro Victor <pedro20002victor@gmail.com>
2022-09-26 23:19:09 -0300

    README UPDATE

--- script.js
+++ script.js
@@ -3,11 +3,14 @@
 let input_value = document.getElementById('inpt-value');
 
 function searchCep() {
+   console.log(input_value.length)
    
-   if(input_value.value == "" | 0) {
+   if(input_value.value < 0) {
       console.log('Campo está invalido!')
    } else {
       console.log(`${input_value.value}`)
    }
 
 }
+
+// Rever o conteudo acima
